7c42912733a6acd6972b4416074f2bbb69d2a73c
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-14 05:12:45 +0000

    Added Gosper's curve

--- Turtle Fractal Drawer.py
+++ Turtle Fractal Drawer.py
@@ -5,15 +5,96 @@ import math
 from collections.abc import Callable
 from itertools import cycle
 
-def reset():
-    t.reset()
-    t.screen.screensize(canvwidth=500, canvheight=500,  bg="black")
-    t.hideturtle()
-    t.speed(0)
+#fractal
+
+def koch_snowflake(level: int, length: float, rainbow_generator: cycle) -> None:
+    
+    if level == 1:
+        t.pencolor(next(rainbow_generator))
+        t.forward(length)
+    else:
+        koch_snowflake(level-1, length/3, rainbow_generator)
+        t.left(60)
+        koch_snowflake(level-1, length/3, rainbow_generator)
+        t.right(120)
+        koch_snowflake(level-1, length/3, rainbow_generator)
+        t.left(60)
+        koch_snowflake(level-1, length/3, rainbow_generator)
+
+def koch_start(level: int, length: float, rainbow_generator: cycle) -> None:
+    t.teleport(-length/2, length/3)
+    for _ in range(3):
+        koch_snowflake(level, length, rainbow_generator)
+        t.right(120)
+
+def gen_hilbert(level: int) -> str:
+    if level == 0:
+        return ""
+    else:
+        return "L" + invert_commands(gen_hilbert(level-1)) + "FR" + gen_hilbert(level-1) + "F" + gen_hilbert(level-1) + "RF" + invert_commands(gen_hilbert(level-1)) + "L"
+
+def hilbert_curve(level: int, curve_size: float, rainbow_generator: cycle) -> None:
+    t.teleport(-curve_size/2, -curve_size/2)
+    step_length = curve_size/((2**level)-1)
+    draw_curve_has_f(gen_hilbert(level), step_length, rainbow_generator)
+
+def gen_dragon(level: int) -> str:
+    if level == 1:
+        return "R"
+    else:
+        return gen_dragon(level-1) + "R" + invert_commands(gen_dragon(level-1))[::-1]
+    
+def dragon_curve(level: int, size: float, rainbow_generator: cycle) -> None:
+    draw_curve_no_f(gen_dragon(level), size/(math.sqrt(2)*(level**math.sqrt(2))), rainbow_generator)
+
+def sierpinski_gasket(level: int, length: float, rainbow_generator: cycle, flipped=1) -> None:
+    
+    if level == 1:
+        t.pencolor(next(rainbow_generator))
+        t.forward(length)
+    else:
+        t.left(60*flipped)
+        sierpinski_gasket(level-1, length/2, rainbow_generator, -flipped)
+        t.right(60*flipped)
+        sierpinski_gasket(level-1, length/2, rainbow_generator, flipped)
+        t.right(60*flipped)
+        sierpinski_gasket(level-1, length/2, rainbow_generator, -flipped)
+        t.left(60*flipped)
+
+def sierpinski_start(level: int, length: float, rainbow_generator) -> None:
+    t.teleport(-length/2, -length/3)
+    sierpinski_gasket(level, length, rainbow_generator)
+
+def gen_gosper(level: int) -> str:
+    if level == 1:
+        return "A"
+    return substitute(gen_gosper(level-1), {"A": "A-B--B+A++AA+B-", "B": "+A-BB--B-A++A+B"})
+
+def draw_gosper_curve(level: int, size: float, rainbow_generator: cycle) -> None:
+    t.teleport(0, size/4)
+    commands = gen_gosper(level)
+    step = size/math.sqrt(7)**(level)
+    for c in commands:
+        t.pencolor(next(rainbow_generator))
+        if c == "A" or c == "B":
+            t.forward(step)
+        elif c == "+":
+            t.left(60)
+        elif c == "-":
+            t.right(60)
+
+# 0 2 8 24
+#  2 6 16
+#   4 10
+#    6
+
+#utility
+
+def substitute(commands: str, rules: dict[str, str]):
+    return "".join(rules[c] if c in rules else c for c in commands)
 
 def invert_commands(commands: str) -> str:
-    swap = {"R": "L", "L": "R", "F": "F"}
-    return "".join(swap[c] for c in commands)
+    return substitute(commands, {"R": "L", "L": "R", "F": "F"})
 
 def draw_curve_has_f(commands: str, step_length: float, rainbow_generator: cycle) -> None:
     for c in commands:
@@ -35,66 +116,6 @@ def draw_curve_no_f(commands: str, step_length: float, rainbow_generator: cycle)
             t.left(90)
         t.pencolor(next(rainbow_generator))
         t.forward(step_length)
-            
-
-def koch_curve(level: int, length: float, rainbow_generator: cycle) -> None:
-    
-    if level == 1:
-        t.pencolor(next(rainbow_generator))
-        t.forward(length)
-    else:
-        koch_curve(level-1, length/3, rainbow_generator)
-        t.left(60)
-        koch_curve(level-1, length/3, rainbow_generator)
-        t.right(120)
-        koch_curve(level-1, length/3, rainbow_generator)
-        t.left(60)
-        koch_curve(level-1, length/3, rainbow_generator)
-
-def koch_snowflake(level: int, length: float, rainbow_generator: cycle) -> None:
-    t.teleport(-length/2, length/3)
-    for _ in range(3):
-        koch_curve(level, length, rainbow_generator)
-        t.right(120)
-
-def hilbert(level: int) -> str:
-    if level == 0:
-        return ""
-    else:
-        return "L" + invert_commands(hilbert(level-1)) + "FR" + hilbert(level-1) + "F" + hilbert(level-1) + "RF" + invert_commands(hilbert(level-1)) + "L"
-
-def hilbert_curve(level: int, curve_size: float, rainbow_generator: cycle) -> None:
-    t.teleport(-curve_size/2, -curve_size/2)
-    step_length = curve_size/((2**level)-1)
-    draw_curve_has_f(hilbert(level), step_length, rainbow_generator)
-
-def dragon(level: int) -> str:
-    if level == 1:
-        return "R"
-    else:
-        return dragon(level-1) + "R" + invert_commands(dragon(level-1))[::-1]
-    
-def dragon_curve(level: int, size: float, rainbow_generator: cycle) -> None:
-    draw_curve_no_f(dragon(level), size/(math.sqrt(2)*(level**math.sqrt(2))), rainbow_generator)
-
-def sierpinski_gasket(level: int, length: float, rainbow_generator: cycle, flipped=1) -> None:
-    
-    if level == 1:
-        t.pencolor(next(rainbow_generator))
-        t.forward(length)
-    else:
-        t.left(60*flipped)
-        sierpinski_gasket(level-1, length/2, rainbow_generator, -flipped)
-        t.right(60*flipped)
-        sierpinski_gasket(level-1, length/2, rainbow_generator, flipped)
-        t.right(60*flipped)
-        sierpinski_gasket(level-1, length/2, rainbow_generator, -flipped)
-        t.left(60*flipped)
-
-def sierpinski_start(level: int, length: float, rainbow_generator) -> None:
-    t.teleport(-length/2, -length/3)
-    sierpinski_gasket(level, length, rainbow_generator)
-   
 
 def iterate_curve(curve: Callable[[int, float, cycle], None], max_iterations: int, size: float, col_list: list[str]) -> None:
     for i in range(1, max_iterations+1):
@@ -102,10 +123,19 @@ def iterate_curve(curve: Callable[[int, float, cycle], None], max_iterations: in
         curve(i, size, cycle(col_list))
         sleep(1)
 
-curves = {1: koch_snowflake, 2: hilbert_curve, 3: dragon_curve, 4: sierpinski_start}
+#UI
+
+def reset():
+    t.reset()
+    t.screen.screensize(canvwidth=500, canvheight=500,  bg="black")
+    t.hideturtle()
+    t.speed(0)
+
+
+curves = {1: koch_start, 2: hilbert_curve, 3: dragon_curve, 4: sierpinski_start, 5: draw_gosper_curve}
 curvesno = None
 while not curvesno:
-    curvesno = simpledialog.askinteger("Select fractal", "What curve do you want to display?\n1) The Koch Snowflake\n2) The Hilbert Curve\n3) The Dragon Curve\n4) Sierpiński gasket", minvalue=1, maxvalue=4)
+    curvesno = simpledialog.askinteger("Select fractal", "What curve do you want to display?\n1) The Koch Snowflake\n2) The Hilbert Curve\n3) The Dragon Curve\n4) Sierpiński gasket\n5) Gosper curve", minvalue=1, maxvalue=5)
 curve = curves[curvesno]
 
 max_iterations = None
