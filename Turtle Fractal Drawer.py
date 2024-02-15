f59e3ee21e612623a93bbf6b74d2ee7befaf3979
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-15 19:37:26 +0000

    created a standard function to draw fractals

--- Turtle Fractal Drawer.py
+++ Turtle Fractal Drawer.py
@@ -9,105 +9,51 @@ import sys
 #fractal
 
 def koch_snowflake(level: int, length: float, rainbow_generator: cycle) -> None:
-    step_length = length/(3**(level-1))
-    commands = l_system(level, "F", {"F": "F+F--F+F"})
-    for c in commands:
-        if c == "F":
-            t.pencolor(next(rainbow_generator))
-            t.forward(step_length)
-        elif c == "-":
-            t.right(60)
-        elif c == "+":
-            t.left(60)
-
-
-def koch_start(level: int, length: float, rainbow_generator: cycle) -> None:
     t.teleport(-length/2, length/3)
+    step_length = length/(3**(level-1))
+    commands = l_system(level, "F", {"F": "F-F++F-F"})
     for _ in range(3):
-        koch_snowflake(level, length, rainbow_generator)
+        draw_l(commands, "F", 60, rainbow_generator, step_length)
         t.right(120)
 
 def hilbert_curve(level: int, curve_size: float, rainbow_generator: cycle) -> None:
     t.teleport(-curve_size/2, -curve_size/2)
     step_length = curve_size/((2**level)-1)
-    commands = l_system(level, "+BF-AFA-FB+", {"A": "+BF-AFA-FB+", "B": "-AF+BFB+FA-"})
-    for c in commands:
-        if c == "-":
-            t.right(90)
-        elif c == "+":
-            t.left(90)
-        elif c =="F":
-            t.pencolor(next(rainbow_generator))
-            t.forward(step_length)
+    commands = l_system(level, "-BF+AFA+FB-", {"A": "-BF+AFA+FB-", "B": "+AF-BFB-FA+"})
+    draw_l(commands, "F", 90, rainbow_generator, step_length)
     
 def dragon_curve(level: int, size: float, rainbow_generator: cycle) -> None:
-    
     step_length = size/(math.sqrt(2)*(level**math.sqrt(2)))
-    commands = l_system(level, "F", {"F": "F+G", "G": "F-G"})
-    for c in commands:
-        if c == "-":
-            t.right(90)
-        elif c == "+":
-            t.left(90)
-        elif c == "F" or c == "G":
-            t.pencolor(next(rainbow_generator))
-            t.forward(step_length)
+    commands = l_system(level, "F", {"F": "F-G", "G": "F+G"})
+    draw_l(commands, "FG", 90, rainbow_generator, step_length)
 
 def sierpinski_gasket(level: int, length: float, rainbow_generator: cycle) -> None:
     t.teleport(-length/2, -length/3)
     if level % 2 == 0:
         t.left(60)
     step_length = length/(2**(level-1))
-    commands = l_system(level, "XF", {"X": "YF-XF-Y", "Y": " XF+YF+X"})
-    for c in commands:
-        if c == "-":
-            t.right(60)
-        elif c == "+":
-            t.left(60)
-        elif c == "F":
-            t.pencolor(next(rainbow_generator))
-            t.forward(step_length)
+    commands = l_system(level, "XF", {"X": "YF+XF+Y", "Y": " XF-YF-X"})
+    draw_l(commands, "F", 60, rainbow_generator, step_length)
 
 def draw_gosper_curve(level: int, size: float, rainbow_generator: cycle) -> None:
     t.teleport(0, size/4)
-    commands = l_system(level, "A", {"A": "A-B--B+A++AA+B-", "B": "+A-BB--B-A++A+B"})
-    step = size/math.sqrt(7)**(level)
-    for c in commands:
-        if c == "A" or c == "B":
-            t.pencolor(next(rainbow_generator))
-            t.forward(step)
-        elif c == "+":
-            t.left(60)
-        elif c == "-":
-            t.right(60)
+    step_length = size/math.sqrt(7)**(level)
+    commands = l_system(level, "A", {"A": "A+B++B-A--AA-B+", "B": "-A+BB++B+A--A-B"})
+    draw_l(commands, "AB", 60, rainbow_generator, step_length)
 
 def moore_curve(level: int, curve_size: float, rainbow_generator: cycle) -> None:
     step_length = curve_size/((2**level)-1)
     t.teleport(-step_length/2, -curve_size/2)
     t.left(90)
     commands = l_system(level, "LFL+F+LFL", {"L": "-RF+LFL+FR-", "R": "+LF-RFR-FL+"})
-    for c in commands:
-        if c == "F":
-            t.pencolor(next(rainbow_generator))
-            t.forward(step_length)
-        elif c == "+":
-            t.right(90)
-        elif c == "-":
-            t.left(90)
+    draw_l(commands, "F", 90, rainbow_generator, step_length)
 
 def peano_curve(level: int, curve_size: float, rainbow_generator: cycle) -> None:
     step_length = curve_size/((3**level)-1)
     t.teleport(-curve_size/2, -curve_size/2)
     t.left(90)
     commands = l_system(level, "XFYFX+F+YFXFY-F-XFYFX", {"X": "XFYFX+F+YFXFY-F-XFYFX", "Y": "YFXFY-F-XFYFX+F+YFXFY"})
-    for c in commands:
-        if c == "F":
-            t.pencolor(next(rainbow_generator))
-            t.forward(step_length)
-        elif c == "+":
-            t.right(90)
-        elif c == "-":
-            t.left(90)
+    draw_l(commands, "F", 90, rainbow_generator, step_length)
 
 #utility
 
@@ -119,6 +65,17 @@ def l_system(level: int, axiom: str, rules: dict[str, str]) -> str:
         return axiom
     else:
         return substitute(l_system(level-1, axiom, rules), rules)
+
+def draw_l(commands: str, forward: str, angle: int, rainbow_generator: cycle, step_length: float):
+    for c in commands:
+        if c in forward:
+            t.pencolor(next(rainbow_generator))
+            t.forward(step_length)
+        elif c == "+":
+            t.right(angle)
+        elif c == "-":
+            t.left(angle)
+
 
 def iterate_curve(curve: Callable[[int, float, cycle], None], max_iterations: int, size: float, col_list: list[str]) -> None:
     for i in range(1, max_iterations+1):
@@ -136,7 +93,7 @@ def reset():
 
 
 curves = {
-    1: (koch_start, "The Koch Snowflake"), 
+    1: (koch_snowflake, "The Koch Snowflake"), 
     2: (hilbert_curve, "The Hilbert Curve"), 
     3: (dragon_curve, "The Dragon Curve"), 
     4: (sierpinski_gasket, "The Sierpiński gasket"), 
