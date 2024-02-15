5c6cfe39807d23f70787891d8a718d4d5256c824
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-02-15 20:31:54 +0000

    renamed functions

--- Turtle Fractal Drawer.py
+++ Turtle Fractal Drawer.py
@@ -11,72 +11,72 @@ import sys
 def koch_snowflake(level: int, length: float, rainbow_generator: cycle, t: Turtle) -> None:
     t.teleport(-length/2, length/3)
     step_length = length/(3**(level-1))
-    commands = l_system(level, "F++F++F++", {"F": "F-F++F-F"})
-    draw_l(commands, "F", 60, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "F++F++F++", {"F": "F-F++F-F"})
+    l_system_draw(commands, "F", 60, rainbow_generator, step_length, t)
 
 def quadratic_koch_curve(level: int, length: float, rainbow_generator: cycle, t: Turtle) -> None:
     t.teleport(-length/2, 0)
     step_length = length/(3**(level-1))
-    commands = l_system(level, "F", {"F": "F-F+F+F-F"})
-    draw_l(commands, "F", 90, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "F", {"F": "F-F+F+F-F"})
+    l_system_draw(commands, "F", 90, rainbow_generator, step_length, t)
 
 def hilbert_curve(level: int, curve_size: float, rainbow_generator: cycle, t: Turtle) -> None:
     t.teleport(-curve_size/2, -curve_size/2)
     step_length = curve_size/((2**level)-1)
-    commands = l_system(level, "-BF+AFA+FB-", {"A": "-BF+AFA+FB-", "B": "+AF-BFB-FA+"})
-    draw_l(commands, "F", 90, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "-BF+AFA+FB-", {"A": "-BF+AFA+FB-", "B": "+AF-BFB-FA+"})
+    l_system_draw(commands, "F", 90, rainbow_generator, step_length, t)
     
 def dragon_curve(level: int, size: float, rainbow_generator: cycle, t: Turtle) -> None:
     step_length = size/(math.sqrt(2)*(level**math.sqrt(2)))
-    commands = l_system(level, "F", {"F": "F-G", "G": "F+G"})
-    draw_l(commands, "FG", 90, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "F", {"F": "F-G", "G": "F+G"})
+    l_system_draw(commands, "FG", 90, rainbow_generator, step_length, t)
 
 def sierpinski_triangle(level: int, length: float, rainbow_generator: cycle, t: Turtle) -> None:
     t.teleport(-length/2, -length/3)
     step_length = length/(2**(level-1))
-    commands = l_system(level, "F-G-G", {"F": "F-G+F+G-F", "G": "GG"})
-    draw_l(commands, "FG", 120, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "F-G-G", {"F": "F-G+F+G-F", "G": "GG"})
+    l_system_draw(commands, "FG", 120, rainbow_generator, step_length, t)
 
 def sierpinski_arrowhead_curve(level: int, length: float, rainbow_generator: cycle, t: Turtle) -> None:
     t.teleport(-length/2, -length/3)
     if level % 2 == 0:
         t.left(60)
     step_length = length/(2**(level-1))
-    commands = l_system(level, "XF", {"X": "YF+XF+Y", "Y": "XF-YF-X"})
-    draw_l(commands, "F", 60, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "XF", {"X": "YF+XF+Y", "Y": "XF-YF-X"})
+    l_system_draw(commands, "F", 60, rainbow_generator, step_length, t)
 
 def draw_gosper_curve(level: int, size: float, rainbow_generator: cycle, t: Turtle) -> None:
     t.teleport(0, size/4)
     step_length = size/math.sqrt(7)**(level)
-    commands = l_system(level, "A", {"A": "A+B++B-A--AA-B+", "B": "-A+BB++B+A--A-B"})
-    draw_l(commands, "AB", 60, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "A", {"A": "A+B++B-A--AA-B+", "B": "-A+BB++B+A--A-B"})
+    l_system_draw(commands, "AB", 60, rainbow_generator, step_length, t)
 
 def moore_curve(level: int, curve_size: float, rainbow_generator: cycle, t: Turtle) -> None:
     step_length = curve_size/((2**level)-1)
     t.teleport(-step_length/2, -curve_size/2)
     t.left(90)
-    commands = l_system(level, "LFL+F+LFL", {"L": "-RF+LFL+FR-", "R": "+LF-RFR-FL+"})
-    draw_l(commands, "F", 90, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "LFL+F+LFL", {"L": "-RF+LFL+FR-", "R": "+LF-RFR-FL+"})
+    l_system_draw(commands, "F", 90, rainbow_generator, step_length, t)
 
 def peano_curve(level: int, curve_size: float, rainbow_generator: cycle, t: Turtle) -> None:
     step_length = curve_size/((3**level)-1)
     t.teleport(-curve_size/2, -curve_size/2)
     t.left(90)
-    commands = l_system(level, "XFYFX+F+YFXFY-F-XFYFX", {"X": "XFYFX+F+YFXFY-F-XFYFX", "Y": "YFXFY-F-XFYFX+F+YFXFY"})
-    draw_l(commands, "F", 90, rainbow_generator, step_length, t)
+    commands = l_system_gen(level, "XFYFX+F+YFXFY-F-XFYFX", {"X": "XFYFX+F+YFXFY-F-XFYFX", "Y": "YFXFY-F-XFYFX+F+YFXFY"})
+    l_system_draw(commands, "F", 90, rainbow_generator, step_length, t)
 
 #utility
 
 def substitute(commands: str, rules: dict[str, str]) -> str:
     return "".join(rules[c] if c in rules else c for c in commands)
 
-def l_system(level: int, axiom: str, rules: dict[str, str]) -> str:
+def l_system_gen(level: int, axiom: str, rules: dict[str, str]) -> str:
     if level == 1:
         return axiom
     else:
-        return substitute(l_system(level-1, axiom, rules), rules)
+        return substitute(l_system_gen(level-1, axiom, rules), rules)
 
-def draw_l(commands: str, forward: str, angle: int, rainbow_generator: cycle, step_length: float, t: Turtle):
+def l_system_draw(commands: str, forward: str, angle: int, rainbow_generator: cycle, step_length: float, t: Turtle):
     for c in commands:
         if c in forward:
             t.pencolor(next(rainbow_generator))
