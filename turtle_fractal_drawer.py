b2abc211d5435a73a2a99751a977dcacebbfdfe0
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-04-03 16:32:43 +0100

    made length a global constant

--- turtle_fractal_drawer.py
+++ turtle_fractal_drawer.py
@@ -11,53 +11,54 @@ import re
 from typing import Self
 
 
+LENGTH = 500
+
 @dataclass
 class CurveDrawer:
     """class that draws the curve"""
 
-    length: float
     col_list: list[str]
     t: Turtle = field(init=False, default_factory=Turtle)
 
     def koch_snowflake(self, level: int) -> None:
         """The Koch snowflake"""
-        self.t.teleport(-self.length / 2, self.length / 3)
-        step_length: float = self.length / (3 ** (level - 1))
+        self.t.teleport(-LENGTH / 2, LENGTH / 3)
+        step_length: float = LENGTH / (3 ** (level - 1))
         commands = self.l_system_gen("F++F++F++", {"F": "F-F++F-F"}, level)
         self.l_system_draw(commands, "F", 60, step_length)
 
     def quadratic_koch_curve(self, level: int) -> None:
         """The quadratic Koch curve"""
-        self.t.teleport(-self.length / 2, 0)
-        step_length = self.length / (3 ** (level - 1))
+        self.t.teleport(-LENGTH / 2, 0)
+        step_length = LENGTH / (3 ** (level - 1))
         commands = self.l_system_gen("F", {"F": "F-F+F+F-F"}, level)
         self.l_system_draw(commands, "F", 90, step_length)
 
     def cesaro_fractal(self, level: int) -> None:
         """The Cesàro fractal"""
-        self.t.teleport(-self.length / 2, 0)
-        step_length = self.length / (2.5 ** (level - 1))
+        self.t.teleport(-LENGTH / 2, 0)
+        step_length = LENGTH / (2.5 ** (level - 1))
         commands = self.l_system_gen("F++", {"F": "F-F++F-F"}, level)
         self.l_system_draw(commands, "F", 75.52, step_length)
 
     def minkowski_sausage(self, level: int) -> None:
         """The Minkowski sausage"""
-        self.t.teleport(-self.length / 2, 0)
-        step_length = self.length / (4 ** (level - 1))
+        self.t.teleport(-LENGTH / 2, 0)
+        step_length = LENGTH / (4 ** (level - 1))
         commands = self.l_system_gen("F", {"F": "F+F-F-FF+F+F-F"}, level)
         self.l_system_draw(commands, "F", 90, step_length)
 
     def minkowski_island(self, level: int) -> None:
         """The Minkowski sausage"""
-        self.t.teleport(-self.length / 2, self.length / 2)
-        step_length = self.length / (4 ** (level - 1))
+        self.t.teleport(-LENGTH / 2, LENGTH / 2)
+        step_length = LENGTH / (4 ** (level - 1))
         commands = self.l_system_gen("F+F+F+F+", {"F": "F+F-F-FF+F+F-F"}, level)
         self.l_system_draw(commands, "F", 90, step_length)
 
     def hilbert_curve(self, level: int) -> None:
         """The Hilbert Curve"""
-        self.t.teleport(-self.length / 2, -self.length / 2)
-        step_length = self.length / ((2**level) - 1)
+        self.t.teleport(-LENGTH / 2, -LENGTH / 2)
+        step_length = LENGTH / ((2**level) - 1)
         commands = self.l_system_gen(
             "-BF+AFA+FB-", {"A": "-BF+AFA+FB-", "B": "+AF-BFB-FA+"}, level
         )
@@ -65,46 +66,46 @@ class CurveDrawer:
 
     def dragon_curve(self, level: int) -> None:
         """The Dragon Curve"""
-        step_length = self.length / (math.sqrt(2) * (level ** math.sqrt(2)))
+        step_length = LENGTH / (math.sqrt(2) * (level ** math.sqrt(2)))
         commands = self.l_system_gen("F", {"F": "F-G", "G": "F+G"}, level)
         self.l_system_draw(commands, "FG", 90, step_length)
 
     def sierpinski_triangle(self, level: int) -> None:
         """The Sierpiński triangle"""
-        self.t.teleport(-self.length / 2, -self.length / 3)
-        step_length = self.length / (2 ** (level - 1))
+        self.t.teleport(-LENGTH / 2, -LENGTH / 3)
+        step_length = LENGTH / (2 ** (level - 1))
         commands = self.l_system_gen("F-G-G", {"F": "F-G+F+G-F", "G": "GG"}, level)
         self.l_system_draw(commands, "FG", 120, step_length)
 
     def sierpinski_curve(self, level: int) -> None:
         """The Sierpiński curve"""
-        step_length = self.length / (
+        step_length = LENGTH / (
             2 ** (level) + 2 ** (level + 1 / 2) - 1 - 2 * math.sqrt(2)
         )
-        self.t.teleport(-step_length / 2, self.length / 2)
+        self.t.teleport(-step_length / 2, LENGTH / 2)
         commands = self.l_system_gen("F++XF++F++XF", {"X": "XF-G-XF++F++XF-G-X"}, level)
         self.l_system_draw(commands, "FG", 45, step_length)
 
     def sierpinski_square_curve(self, level: int) -> None:
         """The Sierpiński square curve"""
-        step_length = self.length / (2 ** (level + 1) - 3)
-        self.t.teleport(-step_length / 2, self.length / 2)
+        step_length = LENGTH / (2 ** (level + 1) - 3)
+        self.t.teleport(-step_length / 2, LENGTH / 2)
         commands = self.l_system_gen("F+XF+F+XF", {"X": "XF-F+F-XF+F+XF-F+F-X"}, level)
         self.l_system_draw(commands, "FG", 90, step_length)
 
     def sierpinski_arrowhead_curve(self, level: int) -> None:
         """The Sierpiński arrowhead curve"""
-        self.t.teleport(-self.length / 2, -self.length / 3)
+        self.t.teleport(-LENGTH / 2, -LENGTH / 3)
         if level % 2 == 0:
             self.t.left(60)
-        step_length = self.length / (2 ** (level - 1))
+        step_length = LENGTH / (2 ** (level - 1))
         commands = self.l_system_gen("XF", {"X": "YF+XF+Y", "Y": "XF-YF-X"}, level)
         self.l_system_draw(commands, "F", 60, step_length)
 
     def draw_gosper_curve(self, level: int) -> None:
         """The Gosper curve"""
-        self.t.teleport(0, self.length / 4)
-        step_length = self.length / math.sqrt(7) ** (level)
+        self.t.teleport(0, LENGTH / 4)
+        step_length = LENGTH / math.sqrt(7) ** (level)
         commands = self.l_system_gen(
             "A", {"A": "A+B++B-A--AA-B+", "B": "-A+BB++B+A--A-B"}, level
         )
@@ -112,8 +113,8 @@ class CurveDrawer:
 
     def moore_curve(self, level: int) -> None:
         """The Moore curve"""
-        step_length = self.length / ((2**level) - 1)
-        self.t.teleport(-step_length / 2, -self.length / 2)
+        step_length = LENGTH / ((2**level) - 1)
+        self.t.teleport(-step_length / 2, -LENGTH / 2)
         self.t.left(90)
         commands = self.l_system_gen(
             "LFL+F+LFL", {"L": "-RF+LFL+FR-", "R": "+LF-RFR-FL+"}, level
@@ -122,8 +123,8 @@ class CurveDrawer:
 
     def peano_curve(self, level: int) -> None:
         """The Peano curve"""
-        step_length = self.length / ((3**level) - 1)
-        self.t.teleport(-self.length / 2, -self.length / 2)
+        step_length = LENGTH / ((3**level) - 1)
+        self.t.teleport(-LENGTH / 2, -LENGTH / 2)
         self.t.left(90)
         commands = self.l_system_gen(
             "XFYFX+F+YFXFY-F-XFYFX",
@@ -215,7 +216,7 @@ def main():
 
     rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
-    curve_drawer = CurveDrawer(500, rainbow)
+    curve_drawer = CurveDrawer(rainbow)
     curve_drawer.iterate_curve(curve_func, max_iterations)
 
 
